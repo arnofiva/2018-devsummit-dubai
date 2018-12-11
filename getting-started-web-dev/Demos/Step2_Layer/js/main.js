d41cbc36c33e202c8faa8f5473e25cd9309a3ac8
Javier Gutierrez <javier_gutierrez@esri.com>
2018-12-12 01:01:11 +0400

    remove useless view.when + format

--- getting-started-web-dev/Demos/Step2_Layer/js/main.js
+++ getting-started-web-dev/Demos/Step2_Layer/js/main.js
@@ -4,7 +4,7 @@ require([
   "esri/layers/FeatureLayer",
   "esri/views/MapView",
   "dojo/domReady!"
-], function(Map, FeatureLayer, MapView) {
+], function (Map, FeatureLayer, MapView) {
   /******************************************************************
    *
    * Add featurelayers to the map example
@@ -13,14 +13,12 @@ require([
 
   var privateSchoolsPoint = new FeatureLayer({
     // Private Schools centroids
-    url:
-      "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Centroids/FeatureServer/0"
+    url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Centroids/FeatureServer/0"
     // url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/PrivateSchools/FeatureServer/0"
   });
 
   var privateSchoolsPoly = new FeatureLayer({
-    url:
-      "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/PrivateSchoolEnrollmentNoRendering/FeatureServer/0"
+    url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/PrivateSchoolEnrollmentNoRendering/FeatureServer/0"
   });
 
   // Set map's basemap
@@ -35,8 +33,6 @@ require([
     center: [-99.14725260912257, 36.48617178360141]
   });
 
-  view.when(function() {
-    // map.addMany([privateSchoolsPoly, privateSchoolsPoint]);
-    map.add(privateSchoolsPoly);
-  });
+  // map.addMany([privateSchoolsPoly, privateSchoolsPoint]);
+  map.add(privateSchoolsPoly);
 });
--- getting-started-web-dev/Demos/Step3_LayerRenderer/js/main.js
+++ getting-started-web-dev/Demos/Step3_LayerRenderer/js/main.js
@@ -4,7 +4,7 @@ require([
   "esri/layers/FeatureLayer",
   "esri/views/MapView",
   "dojo/domReady!"
-], function(Map, FeatureLayer, MapView) {
+], function (Map, FeatureLayer, MapView) {
   var defaultSym = {
     type: "simple-fill", // autocasts as new SimpleFillSymbol()
     outline: {
@@ -26,37 +26,34 @@ require([
     type: "simple", // autocasts as new SimpleRenderer()
     symbol: defaultSym,
     label: "Private school enrollment ratio",
-    visualVariables: [
-      {
-        type: "color",
-        field: "PrivateEnr",
-        stops: [
-          {
-            value: 0.044,
-            color: "#edf8fb",
-            label: "< 0.044"
-          },
-          {
-            value: 0.059,
-            color: "#b3cde3"
-          },
-          {
-            value: 0.0748,
-            color: "#8c96c6",
-            label: "0.0748"
-          },
-          {
-            value: 0.0899,
-            color: "#8856a7"
-          },
-          {
-            value: 0.105,
-            color: "#994c99",
-            label: "> 0.105"
-          }
-        ]
-      }
-    ]
+    visualVariables: [{
+      type: "color",
+      field: "PrivateEnr",
+      stops: [{
+          value: 0.044,
+          color: "#edf8fb",
+          label: "< 0.044"
+        },
+        {
+          value: 0.059,
+          color: "#b3cde3"
+        },
+        {
+          value: 0.0748,
+          color: "#8c96c6",
+          label: "0.0748"
+        },
+        {
+          value: 0.0899,
+          color: "#8856a7"
+        },
+        {
+          value: 0.105,
+          color: "#994c99",
+          label: "> 0.105"
+        }
+      ]
+    }]
   };
 
   /***********************************
@@ -81,8 +78,7 @@ require([
 
   var privateSchoolsPoint = new FeatureLayer({
     // Private Schools centroids
-    url:
-      "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Centroids/FeatureServer/0",
+    url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Centroids/FeatureServer/0",
     renderer: centroidRenderer
   });
 
@@ -91,8 +87,7 @@ require([
     // layer with rendering
     // url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/OverlaySchools/FeatureServer/0"
     // layer without rendering
-    url:
-      "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/PrivateSchoolEnrollmentNoRendering/FeatureServer/0",
+    url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/PrivateSchoolEnrollmentNoRendering/FeatureServer/0",
     outFields: ["*"],
     opacity: 0.8,
     renderer: renderer
@@ -110,8 +105,6 @@ require([
     center: [-99.14725260912257, 36.48617178360141]
   });
 
-  view.when(function() {
-    map.addMany([privateSchoolsPoly, privateSchoolsPoint]);
-    // map.add(privateSchoolsPoly);
-  });
+  map.addMany([privateSchoolsPoly, privateSchoolsPoint]);
+  // map.add(privateSchoolsPoly);
 });
