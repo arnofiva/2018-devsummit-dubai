b5343564475aeaff927fe0f145a81dd3bf2c51ee
Javier Gutierrez <javier_gutierrez@esri.com>
2018-12-12 19:53:26 +0400

    add underground steps

--- plenary-3d/src/berlin-utilities/config.js
+++ plenary-3d/src/berlin-utilities/config.js
@@ -38,7 +38,7 @@ define([
           qualityProfile: "high",
           environment: {
             lighting: {
-              directShadowsEnabled: true,
+              directShadowsEnabled: false,
               ambientOcclusionEnabled: true
             },
             atmosphereEnabled: true,
@@ -50,7 +50,31 @@ define([
 
         // add a transparency slider
         view.ui.empty("top-left");
-        view.ui.add("slider-container", "top-left");
+        view.ui.add("slider-container", "bottom-left");
+
+        view.when(() => {
+          view.map.layers.forEach(l => {
+            l.when(() => {
+              if (l.title == "Buildings Berlin") {
+                l.opacity = 0.5;
+                l.definitionExpression = "OBJECTID <> 238";
+              }
+            });
+          });
+          view.goTo({
+            "position": {
+              "spatialReference": {
+                "latestWkid": 3857,
+                "wkid": 102100
+              },
+              "x": 1482728.0860918157,
+              "y": 6890484.307468331,
+              "z": 857.141964469105
+            },
+            "heading": 4.816029551165902,
+            "tilt": 50.4107139072403
+          })
+        })
       },
 
       steps: [
@@ -63,23 +87,18 @@ define([
 
         {
 
-          title: "Explore data under ground",
+          title: "Reveal underground data",
 
-          code:
-           `
-  // allow underground navigation
-  webscene.ground.navigationConstraint = {
-    type: "none"
-  };
-
+          code: `
   // update ground opacity
   on("slider-change", function (value){
     webscene.ground.opacity = value;
   });
+
 `,
           before: function () {
             const slider = document.getElementById("opacity-slider");
-            document.getElementById("opacity-slider").addEventListener("input", function(event) {
+            document.getElementById("opacity-slider").addEventListener("input", function (event) {
               const value = parseFloat(event.target.value);
               webscene.ground.opacity = value;
             });
@@ -89,7 +108,44 @@ define([
 
         /////////////////////////////////////////////////////////////////////////////////
         //
-        //  Step 2: Add line measurement tool
+        //  Step 2: Navigate underground
+        //
+        /////////////////////////////////////////////////////////////////////////////////
+
+        {
+
+          title: "Explore data underground",
+
+          code: `
+  // allow underground navigation
+  webscene.ground.navigationConstraint = {
+    type: "none"
+  };
+`,
+          before: function () {
+            view.goTo({
+              "position": {
+                "spatialReference": {
+                  "latestWkid": 3857,
+                  "wkid": 102100
+                },
+                "x": 1482925.1856289608,
+                "y": 6891244.671538544,
+                "z": -316.85717961099
+              },
+              "heading": 1.0534118643196637,
+              "tilt": 121.75366780552754
+            },
+            {
+              speedFactor: 0.25
+            })
+          },
+
+        },
+
+        /////////////////////////////////////////////////////////////////////////////////
+        //
+        //  Step 3: Add line measurement tool
         //
         /////////////////////////////////////////////////////////////////////////////////
 
@@ -102,7 +158,19 @@ const widget = new DirectLineMeasurement3D({ view: view });
 view.ui.add(widget, "top-left");
 `,
           before: function () {
-            view.map.presentation.slides.getItemAt(0).applyTo(view);
+            view.goTo({
+              "position": {
+                "spatialReference": {
+                  "latestWkid": 3857,
+                  "wkid": 102100
+                },
+                "x": 1483002.3052711186,
+                "y": 6891756.947785727,
+                "z": -71.85483561549336
+              },
+              "heading": 4.860752849953107,
+              "tilt": 116.3015874411957
+            });
           },
 
           run: function () {
